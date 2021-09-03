b2af035f593a35746b08af59d263b4bba250d5b6
Maioko <90016326+Maioko@users.noreply.github.com>
2021-09-02 21:07:32 -0500

    Add files via upload

---
+++ ReadMe.txt
@@ -0,0 +1,26 @@
+If you think this is going to permanently kill your pc it won't, it will only give you 5 different options none will break
+so you don't have to be worried about it.
+
+Windows defender will detect it be a virus/malware i promise it's not. all the file does is turn off computer.
+
+
+If found a bug/broken command make the folder a zip file and ill check it out on discord @Maioko#0333
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ TurnOffPC.bat
@@ -0,0 +1,83 @@
+@echo OFF
+
+echo "This is nade By Maioko#0333"
+
+echo "1: Ok" 
+
+SET /p option=Choose one option-
+
+If %option%==1 @echo
+
+ECHO "Some Commands Are Currently Broken."
+ECHO "If found a broken command send it to me on discord @Maioko#0333"
+ECHO "1: Close"
+ECHO "2: Yes"
+
+SET /p option=Choose one option-
+
+If %option%==1 exit
+IF %option%==2 @echo
+
+ECHO "I Didn't Know You Were This Lazy But Choose The Option You'd Like"
+ECHO "1: Logoff"
+ECHO "2: Reboot"
+ECHO "3: Hibernate"
+ECHO "4: Shutdown"
+ECHO "5: Close Window"
+ECHO "6: Disable Rick Rolls"
+ECHO "7: Please Don't kill me.."
+ECHO "8: Fake Virus (May kill your CPU / Memory / RAM / Crash PC)"
+ECHO "9: Fake Virus"
+ECHO "10: Unmute"
+ECHO "11: Kill Discord"
+ECHO "12: Kil Minecraft, but installs discord.js (coding plugin for discord bot)"
+echo "13: Kills Browsers"
+
+SET /p option=Choose one option-
+ 
+
+IF %option%==1 SHUTDOWN /l
+IF %option%==2 SHUTDOWN -r -t 10
+IF %option%==3 SHUTDOWN /h
+IF %option%==4 SHUTDOWN /s /f /t 0
+IF %option%==5 exit 
+IF %option%==6 exit 
+IF %option%==7 SHUTDOWN /s /f /t 0 
+IF %option%==8 start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start exit 
+IF %option%==9 start start start start
+
+@ECHO 
+
+ECHO "I Didn't Know You Were This Lazy But Choose The Option You'd Like"
+ECHO "1: Logoff"
+ECHO "2: Reboot"
+ECHO "3: Hibernate"
+ECHO "4: Shutdown"
+ECHO "5: Close Window"
+ECHO "6: Disable Rick Rolls"
+ECHO "7: Please Don't kill me.."
+ECHO "8: Fake Virus (May kill your CPU / Memory / RAM)"
+ECHO "9: Fake Virus"
+ECHO "10: Unmute"
+ECHO "11: Kill Discord"
+ECHO "12: Kil Minecraft, but installs discord.js (coding plugin for discord bot)"
+echo "13: Kills Browsers Open Spotify"
+SET /p option=Choose one option-
+
+IF %option%==1 SHUTDOWN /l
+IF %option%==2 SHUTDOWN -r -t 10
+IF %option%==3 SHUTDOWN /h
+IF %option%==4 SHUTDOWN /s /f /t 0
+IF %option%==5 exit 
+IF %option%==6 exit 
+IF %option%==7 SHUTDOWN /s /f /t 0 
+IF %option%==8 start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start start exit 
+IF %option%==9 start start start start
+IF %option%==10 ECHO Unmute 
+taskkill
+IF %option%==11 taskkill /im discord.exe
+IF %option%==12 taskkill /im javaw.exe
+npm install --Discord.js
+IF %option%==13 taskkill /im chrome.exe taskkill /im operagx.exe taskkill open spotify.exe
+If %option%==14 taskkill /im spotify
+if %option%==15 taskkill /im discord.exe open discord.exe
